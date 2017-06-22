cb665a1b35cbd1826c58f8d4ff8f20eb37bc5f8f
Shalin Shekhar Mangar <shalin@apache.org>
2017-06-22 18:50:55 +0530

    SOLR-10496: Fix ClassCastException because the SRC_NODE hint is a Set

--- solr/solrj/src/java/org/apache/solr/cloud/autoscaling/Policy.java
+++ solr/solrj/src/java/org/apache/solr/cloud/autoscaling/Policy.java
@@ -32,7 +32,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -379,9 +378,12 @@ public class Policy implements MapWriter {
             }
           }
         }
-        if(hints.get(Hint.SRC_NODE) != null && session.matrix.stream().noneMatch(row -> row.node.equals(hints.get(Hint.SRC_NODE)))){
+        Set<String> srcNodes = (Set<String>) hints.get(Hint.SRC_NODE);
+        if (srcNodes != null && !srcNodes.isEmpty() && session.matrix.stream().noneMatch(row -> srcNodes.contains(row.node))) {
           // the source node is dead so live nodes may not have it
-          session.matrix.add(new Row((String) hints.get(Hint.SRC_NODE), session.getPolicy().params, session.dataProvider));
+          for (String srcNode : srcNodes) {
+            session.matrix.add(new Row(srcNode, session.getPolicy().params, session.dataProvider));
+          }
         }
         session.applyRules();
         originalViolations.addAll(session.getViolations());
