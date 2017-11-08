7ccf122de7d93fbe9a03431e811d0b51f3d11bc4
Andrzej Bialecki <ab@apache.org>
2017-11-08 21:00:56 +0100

    SOLR-11458: WIP: * Add "shared_storage" replica property and fix the check in MoveReplicaCmd. * Attempt to roll-back changes in moveHdfsReplica if some of them fail. * Add a flag "inPlaceMove" to let users (and the autoscaling framework) to decide to * Add missing required params in v2 API. * Catch a few spurious errors in HdfsDirectory. allow in-place HDFS move.

--- solr/core/src/java/org/apache/solr/handler/admin/CollectionsHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/CollectionsHandler.java
@@ -130,6 +130,7 @@ import static org.apache.solr.common.cloud.ZkStateReader.TLOG_REPLICAS;
 import static org.apache.solr.common.params.CollectionAdminParams.COUNT_PROP;
 import static org.apache.solr.common.params.CollectionParams.CollectionAction.*;
 import static org.apache.solr.common.params.CommonAdminParams.ASYNC;
+import static org.apache.solr.common.params.CommonAdminParams.IN_PLACE_MOVE;
 import static org.apache.solr.common.params.CommonAdminParams.WAIT_FOR_FINAL_STATE;
 import static org.apache.solr.common.params.CommonParams.NAME;
 import static org.apache.solr.common.params.CommonParams.VALUE_LONG;
@@ -917,6 +918,7 @@ public class CollectionsHandler extends RequestHandlerBase implements Permission
           CollectionParams.SOURCE_NODE,
           CollectionParams.TARGET_NODE,
           WAIT_FOR_FINAL_STATE,
+          IN_PLACE_MOVE,
           "replica",
           "shard");
     }),
